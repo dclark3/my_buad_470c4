23bc167b0bf0183f999b7593c7b6d3ac4a723ed7
unknown <chrisgarcia001@gmail.com>
2016-09-16 14:45:44 -0400

    Adding more examples

--- python_first_steps/first_mod.py
+++ python_first_steps/first_mod.py
@@ -2,11 +2,21 @@
 #orthoginal
 #reminder this file is a module
 #simpel function that adds 3 numbers 
-def add3 (a, b, c):
-	return a + b + c
-
-def say_hello(person):
-	print("hello " + str(person))
+#def add3 (a, b, c):
+	#return a + b + c
 	
-
-
+# Print a greeting
+#def say_hello(person):
+	#print("hello " + str(person))	
+	#print ('A': " + str(a))
+	#print('B': " +str(b))
+	#print('rest': ' + str(rest))	
+	
+def add_nums (a, b, *rest):
+#This is the manual way
+	# sum = a + b 
+	# for v in rest:
+		# sum += v	
+	# return sum
+	return a + b + sum(list(rest))
+	
--- python_first_steps/test.py
+++ python_first_steps/test.py
@@ -1,5 +1,16 @@
 #this is a test for first_mod module
-from first_mod import *
-print("sum of 3, 5, and 7: " + str(add3(3,5,7))) #first test
-say_hello('Daniel') #test second function
 
+from first_mod import *
+
+#print("sum of 3, 5, and 7: " + str(add3(3,5,7))) #first test
+#say_hello('Daniel') #test second function
+
+print (add_nums(3,5))
+print('----')
+print(add_nums(3,5,7))
+print('---------')
+print(add_nums(2,3,4,5,6,7,8))
+print('---------------')
+extras = [4,5,6,7,8,9]
+print(add_nums(2,3, *extras))
+print(add_nums(*extras))
